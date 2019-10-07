d4e97c54ce5d696d3ac24eb9b01d439d18274484
Muhammad Usama <liontiger49@yahoo.com>
2019-10-08 02:23:36 +0500

    Updates

---
+++ precache-manifest.2652a368683cca6108d48efb2d22bbe6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf9f9557b39f8c13d80aa5590c95136d",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "6633725f7aae7c84c886",
+    "url": "/monster-rolodex/static/css/main.195472f3.chunk.css"
+  },
+  {
+    "revision": "17a473a0232006395343",
+    "url": "/monster-rolodex/static/js/2.7beaaff9.chunk.js"
+  },
+  {
+    "revision": "6633725f7aae7c84c886",
+    "url": "/monster-rolodex/static/js/main.68d6e8f5.chunk.js"
+  },
+  {
+    "revision": "b526a15b42453dbcdb77",
+    "url": "/monster-rolodex/static/js/runtime-main.f88f390d.js"
+  }
+]);
